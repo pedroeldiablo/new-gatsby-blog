32af384d2a9cde7223f879e7a935f55908cc39b0
Peter Williams <pedroeldiablo@yahoo.com>
2020-07-29 17:22:42 +0100

    creating hook for screen size changes and linking to hue rotation

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,14 +5,42 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import PropTypes from 'prop-types'
 import { useStaticQuery, graphql } from 'gatsby'
+import styled from 'styled-components'
+import {useViewport} from '../hooks/useViewport'
 
 import Header from './header'
 import './layout.css'
 
+const backgroundColor = `linear-gradient(217deg, rgba(255,0,0,.8), rgba(255,0,0,0) 70.71%),
+linear-gradient(127deg, rgba(0,255,0,.8), rgba(0,255,0,0) 70.71%),
+linear-gradient(336deg, rgba(0,0,255,.8), rgba(0,0,255,0) 70.71%);`;
+// const currentColor = useViewport().width / 255;
+
+const PageComponent = styled.div`
+background: ${backgroundColor};
+height: 100%;
+width: 100vw;`
+
+
+
 const Layout = ({ children }) => {
+  console.log(useViewport().width);
+
+  const currentColor = useViewport().width / 255;
+  const hueRotation = `hue-rotate(${currentColor}rad)`;
+
+  console.log({hueRotation});
+
+
+
+ 
+
+  console.log("Current color is ", currentColor);
+  
+
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -24,7 +52,7 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
+    <PageComponent style={{filter:hueRotation}}>
       <Header siteTitle={data.site.siteMetadata.title} />
       <div
         style={{
@@ -40,7 +68,7 @@ const Layout = ({ children }) => {
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
       </div>
-    </>
+    </PageComponent>
   )
 }
 
