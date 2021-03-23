a40a4b573881c3a33d6826d9e9e425843d1770f0
Peter Williams <pedroeldiablo@yahoo.com>
2021-03-23 14:26:24 +0000

    update tests inline with linting setting

--- src/components/__tests__/image.js
+++ src/components/__tests__/image.js
@@ -1,13 +1,13 @@
-import React from "react"
-import renderer from "react-test-renderer"
+import React from 'react';
+import renderer from 'react-test-renderer';
 
-import Image from "../image"
+import Image from '../image';
 
-describe("Image", () => {
-  it("renders correctly", () => {
+describe('Image', () => {
+  it('renders correctly', () => {
     const tree = renderer
       .create(<Image alt="Gatsby in Space" filename="gatsby-astronaut.png" />)
-      .toJSON()
-    expect(tree).toMatchSnapshot()
-  })
-})
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
